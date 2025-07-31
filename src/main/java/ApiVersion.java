31c6a8a1d54b719ff5957d876436413eb59e2132
Tai Ngo <tai.ngo@bbv.vn>
2025-07-31 08:44:24 +0000

    Serial requests

---
+++ src/main/java/ApiVersion.java
@@ -0,0 +1,249 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ApiVersion {
+    private short apiKey;
+
+    private short apiVersion;
+
+    private int correlationId;
+
+    private short headerLength;
+
+    private byte[] headerContent = new byte[9];
+
+    private byte tagBuffer;
+
+    private byte bodyLength;
+
+    private byte[] bodyContent = new byte[9];
+
+    private int softwareVersion;
+
+    private DataInputStream dataInputstream;
+
+    private DataOutputStream dataOutputStream;
+
+    public ApiVersion(DataInputStream inputStream)
+    {
+        this.dataInputstream = inputStream;
+    }
+
+    public ApiVersion(DataOutputStream outputStream)
+    {
+        this.dataOutputStream = outputStream;
+    }
+
+    public ApiVersion(DataInputStream inputStream, DataOutputStream outputStream)
+    {
+        this.dataInputstream = inputStream;
+        this.dataOutputStream = outputStream;
+    }
+
+    public void read()
+    {
+        if (dataInputstream == null) {
+            return;
+        }
+        try {
+            setApiKey(dataInputstream.readShort());
+            setApiVersion(dataInputstream.readShort());
+            setCorrelationId(dataInputstream.readInt());
+            setHeaderLength(dataInputstream.readShort());
+            byte[] headerContent = new byte[9];
+            dataInputstream.read(headerContent);
+            setHeaderContent(headerContent);
+            setTagBuffer(dataInputstream.readByte());
+            setBodyLength(dataInputstream.readByte());
+            byte[] bodyContent = new byte[9];
+            dataInputstream.read(bodyContent);
+            setBodyContent(bodyContent);
+            setSoftwareVersion(dataInputstream.readInt());
+            // Skip last tag buffer, already set above
+            dataInputstream.skip(1);
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    public void write()
+    {
+        if (dataOutputStream == null) {
+            return;
+        }
+
+        ByteArrayOutputStream byteArrBodyRes = this.responseBody();
+        try {
+            this.dataOutputStream.writeInt(byteArrBodyRes.size() + 4);
+            this.dataOutputStream.writeInt(this.getCorrelationId());
+            this.dataOutputStream.write(byteArrBodyRes.toByteArray());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    public ByteArrayOutputStream responseBody()
+    {
+        ByteArrayOutputStream byteArr = new ByteArrayOutputStream();
+        DataOutputStream dOut = new DataOutputStream(byteArr);
+
+        try {
+            int code = (this.getApiVersion() < 0 || this.getApiVersion() > 4) ? 35 : 0;
+            // Error code
+            dOut.writeShort(code);
+            // API Version Array
+                // Array Length
+                dOut.writeByte(5);
+                // API Version #1
+                    // API Key
+                    dOut.writeShort(1);
+                    // Min Supported API Version
+                    dOut.writeShort(0);
+                    // Max Supported API Version
+                    dOut.writeShort(17);
+                    // Tag Buffer
+                    dOut.writeByte(this.getTagBuffer());
+
+                // API Version #2
+                    // API Key
+                    dOut.writeShort(18);
+                    // Min Supported API Version
+                    dOut.writeShort(0);
+                    // Max Supported API Version
+                    dOut.writeShort(4);
+                    // Tag Buffer
+                    dOut.writeByte(this.getTagBuffer());
+
+                // API Version #3
+                    // API Key
+                    dOut.writeShort(this.getApiKey());
+                    // Min Supported API Version
+                    dOut.writeShort(0);
+                    // Max Supported API Version
+                    dOut.writeShort(4);
+                    // Tag Buffer
+                    dOut.writeByte(this.getTagBuffer());
+
+                // API Version #4
+                    dOut.writeShort(this.getApiKey());
+                    // Min Supported API Version
+                    dOut.writeShort(0);
+                    // Max Supported API Version
+                    dOut.writeShort(4);
+                    // Tag Buffer
+                    dOut.writeByte(this.getTagBuffer());
+
+            // Throttle Time
+            dOut.writeInt(0);
+
+            // Tag Buffer
+            dOut.writeByte(this.getTagBuffer());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+
+        return byteArr;
+    }
+
+    public void setDataInputStream(DataInputStream dataInputStream)
+    {
+        this.dataInputstream = dataInputStream;
+    }
+
+    public void setDataOutputStream(DataOutputStream dataOutputStream)
+    {
+        this.dataOutputStream = dataOutputStream;
+    }
+
+    private void setApiKey(short apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+
+    private void setApiVersion(short apiVersion)
+    {
+        this.apiVersion = apiVersion;
+    }
+
+    private void setCorrelationId(int correlationId)
+    {
+        this.correlationId = correlationId;
+    }
+
+    private void setHeaderLength(short headerLength)
+    {
+        this.headerLength = headerLength;
+    }
+
+    private void setHeaderContent(byte[] headerContent)
+    {
+        this.headerContent = headerContent;
+    }
+
+    private void setTagBuffer(byte tagBuffer)
+    {
+        this.tagBuffer = tagBuffer;
+    }
+
+    private void setBodyLength(byte bodyLength)
+    {
+        this.bodyLength = bodyLength;
+    }
+
+    private void setBodyContent(byte[] bodyContent)
+    {
+        this.bodyContent = bodyContent;
+    }
+
+    private void setSoftwareVersion(int softwareVersion)
+    {
+        this.softwareVersion = softwareVersion;
+    }
+
+    public short getApiKey()
+    {
+        return this.apiKey;
+    }
+
+    public short getApiVersion()
+    {
+        return this.apiVersion;
+    }
+
+    public int getCorrelationId()
+    {
+        return this.correlationId;
+    }
+
+    public short getHeaderLength()
+    {
+        return this.headerLength;
+    }
+
+    public byte[] getHeaderContent()
+    {
+        return this.headerContent;
+    }
+
+    public byte getTagBuffer()
+    {
+        return this.tagBuffer;
+    }
+
+    public byte getBodyLength()
+    {
+        return this.bodyLength;
+    }
+
+    public byte[] getBodyContent()
+    {
+        return this.bodyContent;
+    }
+
+    public int getSoftwareVersion()
+    {
+        return this.softwareVersion;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -25,115 +23,18 @@ public class Main {
 		// Wait for connection from client.
       	clientSocket = serverSocket.accept();
 
-        // Read
         InputStream inputStream = clientSocket.getInputStream();
-        DataInputStream dIn = new DataInputStream(inputStream);
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        OutputStream outputStream = clientSocket.getOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        ApiVersion apiVersion = new ApiVersion(dataInputStream, dataOutputStream);
 
-        int messageSize = dIn.readInt();
-        System.out.println("Message size: " + messageSize);
-
-        short requestApiKey = dIn.readShort();
-        short requestApiVersion = dIn.readShort();
-        System.out.println("request_api_key: " + requestApiKey);
-        System.out.println("request_api_version: " +requestApiVersion);
-
-        int correlationID = dIn.readInt();
-        System.out.println("correlationID: " + correlationID);
-
-        short clientHeaderLength = dIn.readShort();
-        System.out.println("clientHeaderLength: " + clientHeaderLength);
-
-        byte[] clientHeaderContent = new byte[9];
-        dIn.read(clientHeaderContent);
-        System.out.println("clientHeaderContent: " + Arrays.toString(clientHeaderContent));
-
-        byte tagBuffer = dIn.readByte();
-        System.out.println("tagBuffer: " + tagBuffer);
-
-        // skip clientId on Request body, read above
-        byte clientBodyLength = dIn.readByte();
-        System.out.println("clientBodyLength: " + clientBodyLength);
-
-        byte[] clientBodyContent = new byte[9];
-        dIn.read(clientBodyContent);
-        System.out.println("clientBodyContent: " + Arrays.toString(clientBodyContent));
-
-        byte softwareVersionLength = dIn.readByte();
-        System.out.println("softwareVersionLength: " + softwareVersionLength);
-
-        byte[] softwareVersionContent = new byte[3];
-        dIn.read(softwareVersionContent);
-        System.out.println("softwareVersionContent: " + Arrays.toString(softwareVersionContent));
-
-        // skip tagBuffer on Request body, read above
-        dIn.skip(1);
-
-        // Write
-        ByteArrayOutputStream byteArrBodyRes = new ByteArrayOutputStream();
-        DataOutputStream dOut = new DataOutputStream(byteArrBodyRes);
-
-        // Response body
-        int code = (requestApiVersion < 0 || requestApiVersion > 4) ? 35 : 0;
-        // Error code
-        dOut.writeShort(code);
-
-        // API Version Array
-            // Array Length
-            dOut.writeByte(softwareVersionLength + 1);
-
-            // API Version #1
-                // API Key
-                dOut.writeShort(1);
-                // Min Supported API Version
-                dOut.writeShort(0);
-                // Max Supported API Version
-                dOut.writeShort(17);
-                // Tag Buffer
-                dOut.writeByte(tagBuffer);
-
-            // API Version #2
-                // API Key
-                dOut.writeShort(18);
-                // Min Supported API Version
-                dOut.writeShort(0);
-                // Max Supported API Version
-                dOut.writeShort(4);
-                // Tag Buffer
-                dOut.writeByte(tagBuffer);
-
-            // API Version #3
-                // API Key
-                dOut.writeShort(requestApiKey);
-                // Min Supported API Version
-                dOut.writeShort(0);
-                // Max Supported API Version
-                dOut.writeShort(4);
-                // Tag Buffer
-                dOut.writeByte(tagBuffer);
-
-            // API Version #4
-                dOut.writeShort(requestApiKey);
-                // Min Supported API Version
-                dOut.writeShort(0);
-                // Max Supported API Version
-                dOut.writeShort(4);
-                // Tag Buffer
-                dOut.writeByte(tagBuffer);
-            
-            // Throttle Time
-            dOut.writeInt(0);
-
-            // Tag Buffer
-            dOut.writeByte(tagBuffer);
-
-            OutputStream outputStream = clientSocket.getOutputStream();
-            DataOutputStream dOutStream = new DataOutputStream(outputStream);
-            // Get response message size
-            dOutStream.writeInt(byteArrBodyRes.size() + 4);
-            dOutStream.writeInt(correlationID);
-            dOutStream.write(byteArrBodyRes.toByteArray());
-
-
+        while ((dataInputStream.read()) != -1) {
+            // Skip next three byte belong to message size
+            dataInputStream.skip(3);
+            apiVersion.read();
+            apiVersion.write();
+        }
     } catch (IOException e) {
       	System.out.println("IOException: " + e.getMessage());
     } finally {
